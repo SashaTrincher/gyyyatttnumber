d9c24832305ffe277c956547620e242993890d52
SashaTrincher <oofman@ukr.net>
2024-01-01 23:51:51 +0100

    first commit

---
+++ cube.js
@@ -0,0 +1,42 @@
+const trigger = document.getElementById('triggerNumber');
+const container = document.querySelector('.cube');
+const target = document.querySelector('.number');
+
+let isPlaying = false;
+
+
+
+function generateNumber () {
+    
+    if (isPlaying !== true) {
+        isPlaying = true;
+
+        console.log(isPlaying);
+
+        const randomNumber = Math.floor(Math.random(1) * 6);
+
+        if (randomNumber === 0) {
+            randomNumber++
+        }
+
+        target.style.fontSize = "18px";
+        target.innerHTML = '';
+
+        container.classList.add('animation');
+        target.innerHTML = 'generating...'
+
+        setTimeout(() => {
+            container.classList.remove('animation');
+            target.style.fontSize = "40px";
+            target.innerHTML = randomNumber;
+
+            if (randomNumber === 6) {
+                alert('svyat is annoying bitch')
+            }
+
+            isPlaying = false;
+
+            console.log(isPlaying);
+        }, 1300);
+    }
+};
